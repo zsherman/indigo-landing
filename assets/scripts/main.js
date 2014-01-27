991b5c9cb1e8dc48dace1da1e703f31375ede33a
Zach Sherman <zksherm@gmail.com>
2014-01-27 01:41:04 -0600

    sign up input

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,7 +4,21 @@ $(document).foundation();
 
 $(document).ready(function () {
 
+  $('.signup').click(function(e) {
+    $(this).stop().animate({width: "50%"}, 300);
+    $(this).attr("placeholder", "example@gmail.com");
+  });
 
+  $(document).click(function (e) {
+    var container = $(".signup");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.stop().animate({width: "146px"}, 300);
+        container.attr("placeholder", "Get Notified");
+    }
+  });
 
 
 });
